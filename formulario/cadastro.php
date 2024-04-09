414a3f5bd753b1000bb7fa03a49f17b167e64032
Leobeira <163762160+Leobeira@users.noreply.github.com>
2024-04-09 11:12:11 +0100

    exercicios

---
+++ formulario/cadastro.php
@@ -0,0 +1,4 @@
+Os dados foram enviados!
+Aprenda PHP para saber o que fazer com eles.
+
+<!--Utilizamos isso somente para que quando nos formulários, ao clicarmos em enviar, não aparecer o erro da página e sim esta mensagem àcima.-->
